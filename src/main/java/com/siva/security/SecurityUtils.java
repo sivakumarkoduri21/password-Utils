a3a510a79278790a52c4d2e104c34d34fdb63c07
siva kumar <{emailaddress}>
2021-05-15 19:46:34 +0530

    first commit

---
+++ src/main/java/com/siva/security/SecurityUtils.java
@@ -0,0 +1,27 @@
+package com.siva.security;
+
+import java.util.Base64;
+import java.util.Base64.Decoder;
+import java.util.Base64.Encoder;
+
+public class SecurityUtils {
+
+	public static String encode(String plainText) {
+		Encoder encoder = Base64.getEncoder();
+		return encoder.encodeToString(plainText.getBytes());
+	}
+
+	public static String decode(String planinText) {
+
+		Decoder decoder = Base64.getDecoder();
+		byte[] decoderText = decoder.decode(planinText);
+		return new String(decoderText);
+	}
+	
+	public static void main(String[] args) {
+		String s=SecurityUtils.encode("sivakumar");
+		System.out.println(s);
+		String s1=SecurityUtils.decode("c2l2YWt1bWFy");
+		System.out.println(s1);
+	}
+}
